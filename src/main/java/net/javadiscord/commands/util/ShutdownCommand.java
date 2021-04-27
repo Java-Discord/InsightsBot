56501bc05ae17f22e540a68afebafef7d00b6800
Andrew Lalis <andrewlalisofficial@gmail.com>
2021-04-27 15:42:56 +0200

    Added flush of cached data on shutdown.

--- src/main/java/net/javadiscord/commands/util/ShutdownCommand.java
+++ src/main/java/net/javadiscord/commands/util/ShutdownCommand.java
@@ -4,7 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.Message;
 import net.javadiscord.InsightsBot;
 import net.javadiscord.commands.Command;
+import net.javadiscord.data.DataSourceProvider;
+import net.javadiscord.data.GuildDataWriter;
+import net.javadiscord.data.jobs.FullCacheFlushJob;
 import net.javadiscord.util.AdminChecker;
+import org.quartz.JobExecutionContext;
+
+import java.sql.SQLException;
 
 /**
  * Special command to shut down the bot. Only admins may use it.
@@ -19,6 +25,12 @@ public class ShutdownCommand implements Command {
 		}
 		log.info("Shutting down due to shutdown command issued by {}.", message.getAuthor().getAsTag());
 		message.reply("Shutting down now.").complete();
+		try (GuildDataWriter writer = new GuildDataWriter()) {
+			log.info("Flushing data on shutdown.");
+			writer.saveAllGuildData(InsightsBot.get().getGuildsCache().getAll());
+		} catch (SQLException e) {
+			log.error("Could not flush data on shutdown.", e);
+		}
 		InsightsBot.get().getJda().shutdownNow();
 		InsightsBot.get().getJobManager().shutdown();
 	}
